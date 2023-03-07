2ba7af8ed7d8be77079c65d7c80b5d104e78dc6e
Simenya Jonathan Enoch <62070508+Simenya@users.noreply.github.com>
2023-03-07 09:52:39 +0300

    Add files via upload

---
+++ js/activatePuzzle.js
@@ -0,0 +1,66 @@
+
+
+var options = {
+    solution: 			[[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,    "F",	null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,    "L",   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,    "O",   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	 "D",	null,	null,	null,   null,    "O",   null,   null,    "F",   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	 "I",	 "N",	 "T",    "E",    "G",    "R",    "A",    "T",    "I",    "O",     "N",  null,  null,   null,   null],
+                         [null,	null,	null,	null,	null,	 'S',	null,	null,	null,   null,   null,   null,   null,    "B",   null,    null,  null,   null,   null,   null],
+                         [null,	null,	'M',	null,	null,	 'C',   null,   null,	null,   null,   null,   null,   null,    "O",   null,    null,  null,   null,   null,   null],
+                         [null,	'L',	'O',	'G',	'A',	 'R',	'I',	'T',	'H',   'M',     null,   null,   null,    'N',   null,    null,   null,   null,   null,   null],
+                         [null,	null,	'D',	null,	null,	 'E',	null,	null,	null,   null,   null,   null,   null,    'A',   null,   null,   null,   null,   null,   null],
+                         [null,	null,	'U',	null,	null,	 'T',	null,	null,	null,   null,   null,   null,   null,    'C',   null,   null,   null,   null,   null,   null],
+                         [null,	null,	'L',	null,	null,	 'E',	null,	null,	null,   null,   null,   null,   null,    'C',   null,   null,   null,   null,   null,   null],
+                         [null,	null,	'O',	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,    'I',   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]],
+
+
+     index: 			[[null,	  null,	  null,	  null,	  null,	  null,	  null,	  null,	  null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,      4,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,      3,   null,   null,   null,   null,   null,   null,   null,      5,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,      6,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   2,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,      1,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
+                         [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]],
+
+  questions_across: [	  
+                           "1. Power to which a number is supposed to be raised to get the other number",
+                           "6. Finds area under the curve",
+                           
+                     ],
+
+  questions_down: [		 
+                          "2.  Cousin to Division",
+                          "3. Used in cryptography to optmize decision making processes",
+                          "4. Rounds number down to nearest integer",
+                          "5. Spiral squence"
+                 ]
+
+};
+
+
+cwp_nmsp.activateCWP(document.getElementById("myPuzzle"),false,options);
